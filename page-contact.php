34a352ac8795e11cea48f9ae4eabb9d535692758
wachirastudeo <wachirastudeo@gmail.com>
2023-08-07 12:24:53 +0700

    the loop

--- page-contact.php
+++ page-contact.php
@@ -10,17 +10,30 @@
         </div>
     </section>
     <section class="contact-content">
-    <article class="page-content">
+        <article class="page-content">
 
-        <div class="container">
-            
-            <p>We love hearing from our customers, all questions, comments and feedback are always welcome. They help us to ensure that every Starbugs experience you have is the best it can be.</p>
-            <h4>Customer Care</h4>
-            <p>You can call us at +66 0000 0000 from 9.00 a.m. – 6.00 p.m. Monday to Sunday. Alternatively, you can send us an email to customer@coffeecoffee.co.th, and we’ll do our best to reply as quickly as possible.</p>
+            <div class="container">
+                <?php
+                if (have_posts()) {
+                    while (have_posts()) {
+                ?>
+                        <h1> <?php the_title(); ?> </h1>
 
+                    <?php
+                        the_post();
+                        the_content();
+                    }
+                } else {
+                    ?>
+                    <h1>not found</h1>
 
-        </div>
-    </article>
+                <?php
+                }
+
+                ?>
+
+            </div>
+        </article>
     </section>
     <!-- contact form -->
     <section class="contact-form">
@@ -30,7 +43,7 @@
             </h3>
             <p>We will get back to you as soon as possible</p>
             <?php echo do_shortcode('[contact-form-7 id="56" title="Contact us form"]') ?>
-            
+
         </div>
 
     </section>
