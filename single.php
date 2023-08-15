428dc990769d8003229f91e77be7fb9bda22ed50
wachirastudeo <wachirastudeo@gmail.com>
2023-08-15 14:29:38 +0700

    create single.php

---
+++ single.php
@@ -0,0 +1,61 @@
+<?php get_header(); ?>
+<div class="single-wrapper">
+    <div class="container">
+        <!-- breadcrumb -->
+        <ul class="single-breadcrum-list">
+            <li> <a href="#">Home</a></li>
+            <li>Article</li>
+        </ul>
+        <!-- content -->
+
+        <section class="single-content-block">
+            <div class="row">
+                <div class="col-lg-6">
+                    <img src="https://placehold.co/500" alt="cover">
+                </div>
+                <!-- description -->
+
+                <div class="col-lg-6">
+                   <article class="single-main-content page-content">
+                   <h1>Strawberry Jalapeño Mint Julep</h1>
+                    <p>
+                    Don't let Derby Day be your only excuse to make these juleps; they're worthy all year. If you're into spicy margs then you'll be able to handle the heat here; the jalapeño-infused simple syrup mellows out once it hits the cool mint and sweet berries. This recipe makes more simple syrup than you need so save it in the fridge for your next cocktail! 
+                    </p>
+                   </article>
+                </div>
+            </div>
+        </section>
+        <section class="single-relate-block">
+        <h3>Relate posts</h3>
+
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="relate-item">
+                        <img class="relate-image" src="https://placehold.co/300" alt="">
+                        <p class="relate-name"><a href="#"></a></p>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="relate-item">
+                        <img class="relate-image" src="https://placehold.co/300" alt="">
+                        <p class="relate-name"><a href="#"></a></p>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="relate-item">
+                        <img class="relate-image" src="https://placehold.co/300" alt="">
+                        <p class="relate-name"><a href="#"></a></p>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="relate-item">
+                        <img class="relate-image" src="https://placehold.co/300" alt="">
+                        <p class="relate-name"><a href="#"></a></p>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    </div>
+</div>
+<?php get_footer(); ?>
