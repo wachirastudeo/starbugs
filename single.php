5284102788850bbb05e48c37ceb279f62119966e
wachirastudeo <wachirastudeo@gmail.com>
2023-08-16 13:35:21 +0700

    css single page

--- single.php
+++ single.php
@@ -1,4 +1,5 @@
 <?php get_header(); ?>
+
 <div class="single-wrapper">
     <div class="container">
         <!-- breadcrumb -->
@@ -16,43 +17,52 @@
                 <!-- description -->
 
                 <div class="col-lg-6">
-                   <article class="single-main-content page-content">
-                   <h1>Strawberry Jalapeño Mint Julep</h1>
-                    <p>
-                    Don't let Derby Day be your only excuse to make these juleps; they're worthy all year. If you're into spicy margs then you'll be able to handle the heat here; the jalapeño-infused simple syrup mellows out once it hits the cool mint and sweet berries. This recipe makes more simple syrup than you need so save it in the fridge for your next cocktail! 
-                    </p>
-                   </article>
+                    <article class="single-main-content page-content">
+                        <h1>Strawberry Jalapeño Mint Julep</h1>
+                        <p>
+                            Don't let Derby Day be your only excuse to make these juleps; they're worthy all year. If you're into spicy margs then you'll be able to handle the heat here; the jalapeño-infused simple syrup mellows out once it hits the cool mint and sweet berries. This recipe makes more simple syrup than you need so save it in the fridge for your next cocktail!
+                        </p>
+                    </article>
                 </div>
             </div>
         </section>
         <section class="single-relate-block">
-        <h3>Relate posts</h3>
+            <h3>Relate posts</h3>
 
             <div class="row">
                 <div class="col-lg-3">
                     <div class="relate-item">
-                        <img class="relate-image" src="https://placehold.co/300" alt="">
-                        <p class="relate-name"><a href="#"></a></p>
+                        <a href="#" class="relate-image">
+                            <img src="https://placehold.co/300" alt="">
+                        </a>
+                        <p class="relate-name"><a href="#">Classic MaiTai</a></p>
                     </div>
                 </div>
                 <div class="col-lg-3">
                     <div class="relate-item">
-                        <img class="relate-image" src="https://placehold.co/300" alt="">
-                        <p class="relate-name"><a href="#"></a></p>
+                        <a href="#" class="relate-image">
+                            <img src="https://placehold.co/300" alt="">
+                        </a>
+                        <p class="relate-name"><a href="#">Classic MaiTai</a></p>
                     </div>
                 </div>
                 <div class="col-lg-3">
                     <div class="relate-item">
-                        <img class="relate-image" src="https://placehold.co/300" alt="">
-                        <p class="relate-name"><a href="#"></a></p>
+                        <a href="#" class="relate-image">
+                            <img src="https://placehold.co/300" alt="">
+                        </a>
+                        <p class="relate-name"><a href="#">Classic MaiTai</a></p>
                     </div>
                 </div>
                 <div class="col-lg-3">
                     <div class="relate-item">
-                        <img class="relate-image" src="https://placehold.co/300" alt="">
-                        <p class="relate-name"><a href="#"></a></p>
+                        <a href="#" class="relate-image">
+                            <img src="https://placehold.co/300" alt="">
+                        </a>
+                        <p class="relate-name"><a href="#">Classic MaiTai</a></p>
                     </div>
                 </div>
+
 
             </div>
         </section>
