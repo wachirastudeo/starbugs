9208df70365b1c88f3675c5f2e6ac0c1d403957b
wachirastudeo <wachirastudeo@gmail.com>
2023-08-24 12:41:28 +0700

    create social share

--- js/main.js
+++ js/main.js
@@ -44,6 +44,12 @@ jQuery('.menu-slider').slick({
 
 });
 
+function popupWindow(url, windowName, win, w, h) {
+  const y = win.top.outerHeight / 2 + win.top.screenY - ( h / 2);
+  const x = win.top.outerWidth / 2 + win.top.screenX - ( w / 2);
+  return win.open(url, windowName, `toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=${w}, height=${h}, top=${y}, left=${x}`);
+}
+
 jQuery('.mobile-show-menu').on('click',function(){
   jQuery('.mobile-menu-block').addClass('active')
   jQuery('body').addClass('lock')
@@ -52,6 +58,30 @@ jQuery('.mobile-show-menu').on('click',function(){
 jQuery('.mobile-close-menu ,.mobile-barkdrop').on('click',function(){
   jQuery('.mobile-menu-block').removeClass('active')
   jQuery('body').removeClass('lock')
+
+})
+
+jQuery(document).on('click','.social-facebook',function(){
+
+  let url=`https://www.facebook.com/sharer.php?u=${window.location.href}`;
+  // window.location.href
+  popupWindow(url, 'Facebook', window, 600, 300);
+
+})
+
+jQuery(document).on('click','.social-twitter',function(){
+
+  let url=`https://twitter.com/intent/tweet?url=${window.location.href}`;
+  // window.location.href
+  popupWindow(url, 'Twitter', window, 600, 300);
+
+})
+
+jQuery(document).on('click','.social-line',function(){
+
+  let url=`https://lineit.line.me/share/ui?url=${window.location.href}`;
+  // window.location.href
+  popupWindow(url, 'Line', window, 600, 300);
 
 })
 
